0b68be27637baceaba4d6459c24fd48a90fb6854
devchoijih <wlgns3855@gmail.com>
2025-10-14 11:52:41 +0900

    2025_10_14_2

---
+++ 2025_10_14_python/main_2025_10_14_2.py
@@ -0,0 +1,35 @@
+##for문
+
+#for 변수 in range(횟수):
+# print()
+
+"""
+
+for i in range(5):
+    print('반복이 되네 신기해!!!')
+
+for i in range(4,10):
+    print(i)
+
+for i in range(4,10,2):
+    print(i)
+
+for i in reversed(range(4,10)):
+    print(i)
+
+과일 = ["두리안", "망고", "바나나", "멜론"]
+
+# for i in 과일:
+#   print(i)
+
+# for i in "닥스훈트":
+#   print(i, end='')
+
+for i in reversed('닥스훈트'):
+    print(i, end='')
+
+"""
+
+for i in reversed(range(4,10)):
+    print(f'{i} 입니다')
+
