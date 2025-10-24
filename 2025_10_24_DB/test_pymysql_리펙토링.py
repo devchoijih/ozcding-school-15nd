811a8333764a48082db4f2b9ff0862523d08975c
devchoijih <wlgns3855@gmail.com>
2025-10-24 12:05:23 +0900

    2025_10_24_4

--- 2025_10_24_DB/test_pymysql_리펙토링.py
+++ 2025_10_24_DB/test_pymysql_리펙토링.py
@@ -13,7 +13,7 @@ def main():
         host='localhost',
         port=3306,
         user='root',
-        password='0000',
+        password='',
         db='testdatabase',
         charset='utf8mb4',
         cursorclass=pymysql.cursors.DictCursor
