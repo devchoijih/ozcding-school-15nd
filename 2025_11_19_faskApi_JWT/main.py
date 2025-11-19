2d4a8afcfc67605b198a6f0f310ab83b0465d7e0
devchoijih <wlgns3855@gmail.com>
2025-11-19 15:51:44 +0900

    수정

--- 2025_11_19_faskApi_JWT/main.py
+++ 2025_11_19_faskApi_JWT/main.py
@@ -3,15 +3,4 @@ from app.routers.users import user_router
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
-
 app.include_router(user_router)
