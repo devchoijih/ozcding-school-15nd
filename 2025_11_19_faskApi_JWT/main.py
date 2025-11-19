e6219c97445a152dbdde4058e981f38f8b6f353d
devchoijih <wlgns3855@gmail.com>
2025-11-19 15:48:13 +0900

    jwt

---
+++ 2025_11_19_faskApi_JWT/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from app.routers.users import user_router
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+app.include_router(user_router)
