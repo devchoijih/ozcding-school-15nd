d8ba29610c2598784c20cd16ccf13bdd84d7f532
devchoijih <wlgns3855@gmail.com>
2025-12-17 11:36:52 +0900

    2025_12_17 django blog_1

---
+++ 2025_12_17_django_blog/blog/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, get_object_or_404
+
+from blog.models import Blog
+
+# Create your views here.
+
+def blog_list(request):
+    blogs = Blog.objects.all()
+
+    context = {
+        'blogs': blogs,
+    }
+    return render(request, 'blog_list.html', context)
+
+def blog_detail(request, pk):
+    blog = get_object_or_404(Blog, pk=pk)
+    context = {"blog": blog}
+    return render(request, 'blog_detail.html', context)
